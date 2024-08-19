7336fbc20ab00f4068e82edf0d2c3f07ece339c9
MichaelSwain <michaeljswain@gmail.com>
2024-08-19 08:50:51 +0200

    add Employee component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
+import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [role, setRole] = useState('dev');
+    const showEmployees = true;
+    return (
+        <div className="App">
+            {showEmployees ? (
+                <>
+                    <input
+                        type="text"
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                            setRole(e.target.value);
+                        }}
+                    />
+                    <Employee name="Caleb" role="Intern" />
+                    <Employee name="Abby" role={role} />
+                    <Employee name="John" />
+                </>
+            ) : (
+                <p>You cannot see the employees</p>
+            )}
+        </div>
+    );
 }
 
 export default App;
