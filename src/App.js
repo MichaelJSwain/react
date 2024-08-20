4ba963fa2509cfc2cd751f89317e13cea4ca72c4
MichaelSwain <michaeljswain@gmail.com>
2024-08-20 06:01:49 +0200

    render array of employees using map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
     const [role, setRole] = useState('dev');
+    const [employees, setEmployees] = useState(
+        [
+            {
+                name: "Caleb",
+                role: "Developer",
+                img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            },
+            {
+                name: "Abby",
+                role: "Engineer",
+                img: "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            },
+            {
+                name: "John",
+                role: "PO",
+                img: "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            },
+            {
+                name: "Caleb",
+                role: "Developer",
+                img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            },
+            {
+                name: "Abby",
+                role: "Engineer",
+                img: "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            },
+            {
+                name: "John",
+                role: "PO",
+                img: "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            }
+        ]
+    );
+
     const showEmployees = true;
     return (
         <div className="App">
@@ -17,15 +53,16 @@ function App() {
                         }}
                     />
                     <div className="flex flex-wrap justify-center">
-                      <Employee name="Caleb" role="Intern" img="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
-                      <Employee name="Abby" role={role} img="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
-                      <Employee name="John" img="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
-                      <Employee name="Caleb" role="Intern" img="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
-                      <Employee name="Abby" role={role} img="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
-                      <Employee name="John" img="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
-                      <Employee name="Caleb" role="Intern" img="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
-                      <Employee name="Abby" role={role} img="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
-                      <Employee name="John" img="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+                        {employees.map(employee => {
+                            return (
+                                <Employee 
+                                    name={employee.name} 
+                                    role={employee.role} 
+                                    img={employee.img} 
+                                    key={uuidv4()}
+                                />
+                            )
+                        })}
                     </div>
                 </>
             ) : (
