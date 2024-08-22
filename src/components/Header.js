aa0b2bd9bfb2e91694b351cd371e731d1e0fa91e
MichaelSwain <michaeljswain@gmail.com>
2024-08-22 07:16:40 +0200

    remove menu in navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Disclosure, Transition } from '@headlessui/react';
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
 import { NavLink } from 'react-router-dom';
 
@@ -49,12 +49,6 @@ export default function Header({children}) {
                                                 <NavLink
                                                     key={item.name}
                                                     to={item.href}
-                                                    // className={classNames(
-                                                    //     item.current
-                                                    //         ? 'no-underline bg-gray-900 text-white'
-                                                    //         : 'no-underline text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                    //     'px-3 py-2 rounded-md text-sm font-medium'
-                                                    // )}
                                                     className={({isActive}) => {
                                                         console.log(item.href + ' ' + isActive);
                                                         return `px-3 py-2 rounded-md text-sm font-medium no-underline 
@@ -83,74 +77,6 @@ export default function Header({children}) {
                                             aria-hidden="true"
                                         />
                                     </button>
-
-                                    {/* Profile dropdown */}
-                                    <Menu as="div" className="ml-3 relative">
-                                        <div>
-                                            <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                                                <span className="sr-only">
-                                                    Open user menu
-                                                </span>
-                                            </Menu.Button>
-                                        </div>
-                                        <Transition
-                                            as={Fragment}
-                                            enter="transition ease-out duration-100"
-                                            enterFrom="transform opacity-0 scale-95"
-                                            enterTo="transform opacity-100 scale-100"
-                                            leave="transition ease-in duration-75"
-                                            leaveFrom="transform opacity-100 scale-100"
-                                            leaveTo="transform opacity-0 scale-95"
-                                        >
-                                            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(
-                                                                active
-                                                                    ? 'bg-gray-100'
-                                                                    : '',
-                                                                'block px-4 py-2 text-sm text-gray-700'
-                                                            )}
-                                                        >
-                                                            Your Profile
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(
-                                                                active
-                                                                    ? 'bg-gray-100'
-                                                                    : '',
-                                                                'block px-4 py-2 text-sm text-gray-700'
-                                                            )}
-                                                        >
-                                                            Settings
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                                <Menu.Item>
-                                                    {({ active }) => (
-                                                        <a
-                                                            href="#"
-                                                            className={classNames(
-                                                                active
-                                                                    ? 'bg-gray-100'
-                                                                    : '',
-                                                                'block px-4 py-2 text-sm text-gray-700'
-                                                            )}
-                                                        >
-                                                            Sign out
-                                                        </a>
-                                                    )}
-                                                </Menu.Item>
-                                            </Menu.Items>
-                                        </Transition>
-                                    </Menu>
                                 </div>
                             </div>
                         </div>
